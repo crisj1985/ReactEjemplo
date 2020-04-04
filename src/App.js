3bf808484afc17aa25c300a3f282bed60a0050af
crisj1985 <crisj1985@hotmail.com>
2020-04-03 20:25:16 -0500

    version es 6

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-    return ( < div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        p >
-        Edit < code > src / App.js < /code> and to reload.  <
-        /p> <a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        Learn React <
-        /a> </header >
-        <
-        /div>
-    );
-}
+const Hello = (props) => <h2>{props.title}</h2> ;
+
+    // class Hello extends Component
+    // {
+    //     render()
+    //     {
+    //         return <h2>{this.props.title}</h2>;
+    //     }
+    // }
+
+ class App extends Component
+ {
+    render()
+    {
+        return (< div className="App" >
+            < header className="App-header" >
+                < img src={logo}
+                    className="App-logo"
+                    alt="logos" />
+                <Hello title='Hello wordl' />
+
+                < p>
+                    Edit < code > src / App.js </code>  reload.
+        </p> <a className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    Learn React >
+                </a> </header >
+            <p></p>
+        </div>
+        );
+    }
+ }
+// function App() {
+    
+// }
 
 export default App;
